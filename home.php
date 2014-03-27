c2a985ed4668820ada295cd92d14665d5f84cbf9
unknown <m.fjeldwold@gmail.com>
2014-03-27 19:52:39 +0100

    All posts are now listed under blog submenu, link also fixed

--- home.php
+++ home.php
@@ -1,18 +1,25 @@
 <?php get_header(); ?>
-		<div class="container">
-		<div class="row">
-			<div class="col-lg-8 col-md-6">
-				<?php $the_query = new WP_Query( 'showposts=1' ); ?>
-				<?php if ($the_query -> have_posts()) : while ($the_query -> have_posts()) : $the_query->the_post()  ?>
-				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<p><em><?php the_date();?></em></p>
-				
-				<?php the_content(); ?>
-				
-				<?php endwhile; else: ?>
-				<p><?php _e('Beklager det finnes ingen blogposter.'); ?></p>
-				<?php endif; ?>
-			</div><!-- /.col-lg-4 -->
-		</div><!-- /.row -->
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 col-md-6">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h2>
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a>
+			</h2>
+			<p>
+				<em><?php the_time('l, F jS, Y'); ?> </em>
+			</p>
+			<hr>
 
-<?php get_footer(); ?>
+
+			<?php endwhile; else: ?>
+			<p>
+				<?php _e('Sorry, there are no posts.'); ?>
+			</p>
+			<?php endif; ?>
+		</div>
+		<!-- /.col-lg-4 -->
+	</div>
+	<!-- /.row -->
+
+	<?php get_footer(); ?>
